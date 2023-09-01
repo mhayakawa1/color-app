c0f72a16d688daaab9f402b986020589d3c1d3bb
makihaya <93355988+makihaya@users.noreply.github.com>
2023-09-01 14:21:48 -0700

    Create ColorWheel.jsx

---
+++ src/Components/ColorWheel.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function ColorWheel(){
+
+  
+  return(    
+  <div>
+    <h3>Color Wheel</h3>
+  </div>
+  )
+}

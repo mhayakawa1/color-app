29a70737c26f07efcd65a102a87cb375809ba2f8
makihaya <93355988+makihaya@users.noreply.github.com>
2023-09-01 14:21:52 -0700

    Create RandomScheme.jsx

---
+++ src/Components/RandomScheme.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+{/*Goals
+- http://colormind.io/api-access/
+- Fetch random color scheme using API
+- generate new one on button click
+- Save colors, save whole scheme
+*/}
+
+export default function RandomScheme(){
+
+  
+  return(
+  <div>
+    <h3>Random Color Scheme</h3>
+  </div>
+  )
+}

24e38fe0607adec711aa46cccc7824b65d32cae4
makihaya <93355988+makihaya@users.noreply.github.com>
2023-09-01 14:21:34 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import React, {useState} from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import ColorPicker from './Components/ColorPicker';
+import ColorWheel from './Components/ColorWheel';
+import RandomScheme from './Components/RandomScheme';
+import SavedColors from './Components/SavedColors';
+{/*Goals
+  - When button is clicked, component shows
+  - Home - save, edit and delete saved colors
+  - Color Picker - use rgb color picker to find colors
+  - Color Wheel - use color wheel to view different color schemes
+  - Color Randomizer - pick and save random colors using 
+    color scheme API http://colormind.io/api-access/
+
+*/}
+function App() {
+  const [display, setDisplay] = useState(<SavedColors />);  
+  const [savedColors, setSavedColors] = useState([]);
+
+  function switchComponent(comp){
+    setDisplay(comp)
+  }
+
+  return (
+    <div className='App'>
+      <div className='menu'>
+        <button onClick={() => switchComponent(<SavedColors />)}>Saved Colors</button>
+        <button onClick={() => switchComponent(<ColorWheel />)}>Color Wheel</button>
+        <button onClick={() => switchComponent(<ColorPicker />)}>Color Picker</button>
+        <button onClick={() => switchComponent(<RandomScheme />)}>Random Color Scheme</button>
+      </div>
+
+      <div className='component-container'>
+        {display}
+      </div>
+    </div>
+  );
+}
+
+export default App;

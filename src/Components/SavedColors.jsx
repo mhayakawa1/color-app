6e4a76992ed7802c3e0312da932365e16e5d6109
makihaya <93355988+makihaya@users.noreply.github.com>
2023-09-11 17:25:45 -0700

    Update SavedColors.jsx

--- src/Components/SavedColors.jsx
+++ src/Components/SavedColors.jsx
@@ -10,8 +10,8 @@ import { NavLink } from 'react-router-dom';
 export default function SavedColors(){  
   const [savedColors, setSavedColors] = useState('');
   const [showConfirm, setShowConfirm] = useState(false); //
-  const [confirmReset, setConfirmReset] = useState(false);
-  
+  const [lastDeleted, setLastDeleted] = useState('');
+
   const onStorageUpdate = (e) => {
     const { key, newValue } = e;
     if(key === 'savedColors'){
@@ -44,30 +44,35 @@ export default function SavedColors(){
     }
   }
 
-  const deleteColor = (i) => {
-    console.log(i)
-  }
-
   const savedColorsLoop = () => {
-    let splitArr = savedColors.split('*').splice(1);//array from savedColors, split into multiple arrays
+    let splitArr = savedColors.split('*');//array from savedColors, split into multiple arrays
     let itemsArr = []
+
+    const deleteColor = (rgb) => {
+      splitArr = splitArr.filter(elem => elem !== rgb)
+      splitArr = splitArr.join('*')
+      setSavedColors(splitArr)
+      localStorage.setItem('savedColors', splitArr);
+    }
     
     for(let i = 0; i < splitArr.length; i++){
       //replace all elements in splitArr with array made of each element
       splitArr.splice(i, 1, splitArr[i].split(','))
     }
     for(let i = 0; i < splitArr.length; i++){
-      const item = <div key={i} className='saved-color-item'>
+      const item = <div key={`(${splitArr[i][0]}, ${splitArr[i][1]}, ${splitArr[i][2]})`} className='saved-color-item'>
         <div className='saved-color-circle' style={{background: `rgb(${splitArr[i][0]}, ${splitArr[i][1]}, ${splitArr[i][2]})`}}>
           <div className='saved-color-info'>
-            <button className='delete-saved-color' onClick={() => deleteColor(i)}>X</button>
+            <button className='delete-saved-color' onClick={() => deleteColor(splitArr[i])}>X</button>
             <p className='saved-color-value'>{`R ${splitArr[i][0]}`}</p>
             <p className='saved-color-value'>{`G ${splitArr[i][1]}`}</p>
             <p className='saved-color-value'>{`B ${splitArr[i][2]}`}</p>
           </div>
         </div>
       </div>
-      itemsArr.push(item)
+      if(splitArr[i][0] !== ''){
+        itemsArr.push(item)
+      }      
     }
     return(
       itemsArr
@@ -79,7 +84,7 @@ export default function SavedColors(){
     <h3>Your Saved Colors</h3>
     <div className='saved-colors-container'>{savedColorsLoop()}</div>
 
-    <button onClick={() => clearAll('clear all')}>Clear All</button>
+    <button onClick={savedColors === '' ? null : () => clearAll('clear all')}>Clear All</button>
     {showConfirm ? //popup to confirm action to clear all colors
       <div className='confirm-clear-all'>
         <p>Are you sure you want to clear all colors?</p>

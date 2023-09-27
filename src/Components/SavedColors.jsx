95b1ea48185425d090b1a54e3f997952fd609dea
makihaya <93355988+makihaya@users.noreply.github.com>
2023-09-27 10:16:00 -0700

    Update SavedColors.jsx

--- src/Components/SavedColors.jsx
+++ src/Components/SavedColors.jsx
@@ -9,8 +9,9 @@ import { NavLink } from 'react-router-dom';
 
 export default function SavedColors(){
   const [savedColors, setSavedColors] = useState('');
-  const [showConfirm, setShowConfirm] = useState(false); //
-  const [lastDeleted, setLastDeleted] = useState('');
+  const [showConfirm, setShowConfirm] = useState(false);
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+  const [lastDeleted, setLastDeleted] = useState('');//create undo delete color feature?
 
   const onStorageUpdate = (e) => {
     const { key, newValue } = e;
@@ -44,7 +45,47 @@ export default function SavedColors(){
     }
   }
 
+  {/*
+  const rgb2hex = () =>{
+    setRgbCode(`${red},${green},${blue}`)
+    let rgbInput = rgbCode.split(',')
+    let hexOutput = []
+    for(let i = 0; i < rgbInput.length; i++){
+      rgbInput[i] = rgbInput[i]/16
+      rgbInput[i] = rgbInput[i].toString().split('.')
+      rgbInput[i][1] = '.' + rgbInput[i][1]
+      if(rgbInput[i][1] === '.undefined'){
+        hexOutput.push(hex[rgbInput[i][0]], '0')
+      }else{
+        hexOutput.push(hex[rgbInput[i][0]], hex[rgbInput[i][1]*16])
+      }
+    }
+    setHexCode(hexOutput.join(''))
+  }
+*/}
+
   const savedColorsLoop = () => {
+    //convert rgb to hex
+    let rgbInput = savedColors.split('*').splice(1)
+    let hexOutput = []
+    for(let i = 0; i < rgbInput.length; i++){
+      let itemSplit = rgbInput[i].split(',');
+      let itemStr = ''
+      for(let j = 0; j < itemSplit.length; j++){
+        itemSplit[j] = itemSplit[j]/16
+        itemSplit[j] = itemSplit[j].toString().split('.')
+        itemSplit[j][1] = '.' + itemSplit[j][1]
+        if(itemSplit[j][1] === '.undefined'){
+          //hexOutput.push(hex[itemSplit[j][0]], '0')
+          itemStr = itemStr + hex[itemSplit[j][0]] + '0'
+        }else{
+          itemStr = itemStr + hex[itemSplit[j][0]] + hex[itemSplit[j][1]*16]
+          //hexOutput.push(hex[itemSplit[j][0]], hex[itemSplit[j][1]*16])
+        }
+      }
+      hexOutput.push(itemStr.toUpperCase())
+    }
+
     let splitArr = savedColors.split('*');//array from savedColors, split into multiple arrays
     let itemsArr = []
 
@@ -54,7 +95,7 @@ export default function SavedColors(){
       setSavedColors(splitArr)
       localStorage.setItem('savedColors', splitArr);
     }
-    
+    //console.log(splitArr.length, hexOutput.length)
     for(let i = 0; i < splitArr.length; i++){
       //replace all elements in splitArr with array made of each element
       splitArr.splice(i, 1, splitArr[i].split(','))
@@ -62,13 +103,12 @@ export default function SavedColors(){
     for(let i = 0; i < splitArr.length; i++){
       const item = <div key={`(${splitArr[i][0]}, ${splitArr[i][1]}, ${splitArr[i][2]})`} className='saved-color-item'>
         <div className='saved-color-circle' style={{background: `rgb(${splitArr[i][0]}, ${splitArr[i][1]}, ${splitArr[i][2]})`}}>
-          <div className='saved-color-info'>
-            <button className='delete-saved-color' onClick={() => deleteColor(splitArr[i])}>X</button>
-            <p className='saved-color-value'>{`R ${splitArr[i][0]}`}</p>
-            <p className='saved-color-value'>{`G ${splitArr[i][1]}`}</p>
-            <p className='saved-color-value'>{`B ${splitArr[i][2]}`}</p>
-          </div>
         </div>
+        <div className='saved-color-info'>
+            <p className='saved-color-value'>{`RGB(${splitArr[i][0]},${splitArr[i][1]},${splitArr[i][2]})`}</p>
+            <p className='saved-color-value'>#{hexOutput[i-1]}</p>
+            <button className='delete-saved-color' onClick={() => deleteColor(splitArr[i])}>Delete</button>
+          </div>
       </div>   
       if(splitArr[i][0] !== ''){
         itemsArr.push(item)
@@ -78,21 +118,22 @@ export default function SavedColors(){
       itemsArr
     )
   }
-  //console.log(savedColors.split('*').splice(1))
 
   return(    
   <div>
-    <h3>Saved Colors</h3>
-    <div className='saved-colors-container'>{savedColorsLoop()}</div>
+    <div className='controls-container'>
 
+    </div>
     <button onClick={savedColors === '' ? null : () => clearAll('clear all')}>Clear All</button>
-    {showConfirm ? //popup to confirm action to clear all colors
+      {showConfirm ? //popup to confirm action to clear all colors
       <div className='confirm-clear-all'>
         <p>Are you sure you want to clear all colors?</p>
         <button onClick={() => clearAll('yes')}>Yes</button>
         <button onClick={() => clearAll('no')}>No</button>
       </div> 
     : null}
+
+    <div className='saved-colors-container'>{savedColorsLoop()}</div>
   </div>
   )
 }

ae735403bc787e717815ae7bb21032f543ef658a
makihaya <93355988+makihaya@users.noreply.github.com>
2023-10-14 16:47:40 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
 import React, {useState} from 'react';
-
 import ColorPicker from './Components/ColorPicker';
 import ColorWheel from './Components/ColorWheel';
 import RandomScheme from './Components/RandomScheme';
 import SavedColors from './Components/SavedColors';
-{/*Goals
-  - When button is clicked, component shows
-  - Home - save, edit and delete saved colors
-  - Color Picker - use rgb color picker to find colors
-  - Color Wheel - use color wheel to view different color schemes
-  - Color Randomizer - pick and save random colors using 
-    color scheme API http://colormind.io/api-access/
 
-*/}
 function App() {
   const [display, setDisplay] = useState(<SavedColors />);
 
@@ -23,15 +14,20 @@ function App() {
 
   return (
     <div className='app'>
-      <div className='menu'>
-        <button onClick={() => switchComponent(<SavedColors />)} 
-          className={`menu-item-1 ${display.type.name === 'SavedColors' ? 'active' : ''}`}>Saved Colors</button>
-        <button onClick={() => switchComponent(<ColorPicker />)}
-          className={`${display.type.name === 'ColorPicker' ? 'active' : ''}`}>Color Picker</button>
-        <button onClick={() => switchComponent(<RandomScheme />)}
-          className={`${display.type.name === 'RandomScheme' ? 'active' : ''}`}>Color Schemes</button>
-        <button onClick={() => switchComponent(<ColorWheel />)}
-          className={`${display.type.name === 'ColorWheel' ? 'active' : ''}`}>Color Wheel</button>
+      <div className='menu'> 
+        <span className='filler'></span>
+        <div>
+          <button onClick={() => switchComponent(<SavedColors />)}>Saved Colors</button>
+          <button onClick={() => switchComponent(<ColorPicker />)}>Color Picker</button>
+          <button onClick={() => switchComponent(<RandomScheme />)}>Color Schemes</button>
+          <button onClick={() => switchComponent(<ColorWheel />)}>Color Wheel</button>
+        </div>
+        <span className={`active-bar ${
+          display.type.name === 'SavedColors' ? 'menu-item-1'
+          : display.type.name === 'ColorPicker' ? 'menu-item-2'
+          : display.type.name ==='RandomScheme' ? 'menu-item-3'
+          : 'menu-item-4'}`}>
+        </span>
       </div>
 
       <div className='component-container'>

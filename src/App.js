8139023fbc1ee86bea7affa045f72c27eddd4bda
makihaya <93355988+makihaya@users.noreply.github.com>
2023-10-14 19:18:47 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,34 +6,35 @@ import RandomScheme from './Components/RandomScheme';
 import SavedColors from './Components/SavedColors';
 
 function App() {
-  const [display, setDisplay] = useState(<SavedColors />);
+  const [display, setDisplay] = useState('SavedColors');
 
   function switchComponent(comp){
     setDisplay(comp)
   }
-
-  console.log(display)
 
   return (
     <div className='app'>
       <div className='menu'> 
         <span className='filler'></span>
         <div>
-          <button onClick={() => switchComponent(<SavedColors />)}>Saved Colors</button>
-          <button onClick={() => switchComponent(<ColorPicker />)}>Color Picker</button>
-          <button onClick={() => switchComponent(<RandomScheme />)}>Color Schemes</button>
-          <button onClick={() => switchComponent(<ColorWheel />)}>Color Wheel</button>
+          <button onClick={() => switchComponent('SavedColors')}>Saved Colors</button>
+          <button onClick={() => switchComponent('ColorPicker')}>Color Picker</button>
+          <button onClick={() => switchComponent('RandomScheme')}>Color Schemes</button>
+          <button onClick={() => switchComponent('ColorWheel')}>Color Wheel</button>
         </div>
         <span className={`active-bar ${
-          display.type.name === 'SavedColors' ? 'menu-item-1'
-          : display.type.name === 'ColorPicker' ? 'menu-item-2'
-          : display.type.name ==='RandomScheme' ? 'menu-item-3'
+          display === 'SavedColors' ? 'menu-item-1'
+          : display === 'ColorPicker' ? 'menu-item-2'
+          : display ==='RandomScheme' ? 'menu-item-3'
           : 'menu-item-4'}`}>
         </span>
       </div>
 
       <div className='component-container'>
-        {display}
+        {display === 'SavedColors' ? <SavedColors />
+          : display === 'ColorPicker' ? <ColorPicker />
+          : display ==='RandomScheme' ? <RandomScheme />
+          : <ColorWheel />}
       </div>
     </div>
   );

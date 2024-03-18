c345eb774434aca30bc666cd1af4b95485f2e213
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2024-03-17 23:01:30 -0700

    4 changed files

--- src/Components/RandomScheme.jsx
+++ src/Components/RandomScheme.jsx
@@ -1,57 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
 
-{/*https://www.colr.org/api.html*/}
+{/*https://www.colr.org/api.html
+http://colormind.io/api/
+*/}
 
 export default function RandomScheme(){
   const [savedColors, setSavedColors] = useState('');
-  const [schemeArr, setSchemeArr] = useState('');
+  const [schemeArr, setSchemeArr] = useState([]);
+  const [hexCodes, setHexCodes] = useState([]);
   const [rgbArr, setRgbArr] = useState('');
-  const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  const hexVals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
-  const onStorageUpdate = (e) => {
-    const { key, newValue } = e;
-    if(key === 'schemeArr'){
-      setSchemeArr(newValue)
-    }
-  }
-
-  const hex2rgb = (arr) =>{
-    for(let i = 0; i < arr.length; i++){
-      arr[i] = arr[i].split('')
-      for(let j = 0; j < arr[i].length; j++){
-        if (/[0-9]/.test(arr[i][j]) === false) {
-          arr[i].splice(j, 1, hexArr.indexOf(arr[i][j]))
-        }
-      }
-      let arr2 = []
-      for(let k = 0; k < 6; k = k + 2){
-        arr2.push(arr[i][k]*16 + Number(arr[i][k+1]))
-      }
-      arr[i] = arr2
-    }
-    setRgbArr(arr.join('*'));
-    localStorage.setItem('rgbArr', arr.join('*'));
-  }
-
-  function getAPI(){
-    //generate random number from 100 - 17969, insert it into url
-    let randomNum = Math.floor(Math.random() * (17970-100) + 100);
-    fetch(`https://www.colr.org/json/scheme/${randomNum}`)
-    .then(res => res.json())
-    .then(result => {
-        setSchemeArr(result.schemes[0].colors.join(','))
-        localStorage.setItem('schemeArr', result.schemes[0].colors.join(','));
-        hex2rgb(result.schemes[0].colors)
+  const getAPI = () =>{
+    fetch('http://colormind.io/api/', {
+      method: 'POST',
+      body: JSON.stringify({
+        model: 'default'
+      })
     })
-    .catch(err=>console.log(err))
+    .then(res => res.json())
+    .then(res => {
+      setSchemeArr(res.result)
+    })
   }
 
   const saveSchemeColor = (i) =>{
-    if(savedColors.includes(rgbArr.split('*')[i]) === false){
+    {/*if(savedColors.includes(rgbArr.split('*')[i]) === false){
       setSavedColors(savedColors + '*' + rgbArr.split('*')[i])
       localStorage.setItem('savedColors', savedColors + '*' + rgbArr.split('*')[i]);
-    }
+    }*/}
   }
 
   const saveAllColors = () =>{   
@@ -65,20 +42,33 @@ export default function RandomScheme(){
     setSavedColors(savedColors + '*' + arr.join('*'))
     localStorage.setItem('savedColors', savedColors + '*' + arr.join('*'));
   }
- 
+
   const colorSchemeLoop = () => {
     let renderScheme = []
-    let split = schemeArr.split(',')
-    for(let i = 0; i < split.length; i++){
-      if(split.length > 1){     
+    for(let i = 0; i < 5; i++){
+      let color = schemeArr[i]
+      let rgbInput = [...schemeArr[i]]
+      let hexOutput = []
+      for(let j = 0; j < 3; j++){
+        rgbInput[j] = rgbInput[j]/16
+        rgbInput[j] = rgbInput[j].toString().split('.')
+        rgbInput[j][1] = '.' + rgbInput[j][1]
+        if(rgbInput[j][1] === '.undefined'){
+          hexOutput.push(hexVals[rgbInput[j][0]], '0')
+        }else{
+          hexOutput.push(hexVals[rgbInput[j][0]], hexVals[rgbInput[j][1]*16])
+        }
+      }
+      
+      if(schemeArr.length > 1){
         renderScheme.push(
           <div key={i} className='color-item'>
-            <div style={{backgroundColor: `#${split[i]}`}}
+            <div style={{backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`}}
               className='color-block'>
             </div>
             <div className='color-info'>
-              <p className='color-value'>RGB: ({rgbArr.split('*')[i]})</p>
-              <p className='color-value'>HEX: #{split[i].toUpperCase()}</p>
+              <p className='color-value'>RGB: {`${color[0]},${color[1]},${color[2]}`}</p>
+              <p className='color-value'>HEX: #{hexOutput.join('').toUpperCase()}</p>
               <button className='btn-standard btn-small' onClick={() => saveSchemeColor(i)}>Save</button>
             </div>
           </div>
@@ -91,28 +81,18 @@ export default function RandomScheme(){
     )
   }
 
-  useEffect(() => {
-    setSavedColors(localStorage.getItem('savedColors') || '');
-    setSchemeArr(localStorage.getItem('schemeArr') || '');
-    setRgbArr(localStorage.getItem('rgbArr') || '');
-    window.addEventListener('storage', onStorageUpdate);
-      return () => {
-        window.removeEventListener('storage', onStorageUpdate);
-      };
-  },[]);
-
   return(
   <div className='component-container-2'>
     <div className='controls-container scheme-controls-container'>
       <button className='btn-standard' onClick={() => saveAllColors()}>Save All</button>
-      <button className='btn-standard' onClick={() => getAPI()}>New Color Scheme</button>
+      <button className='btn-standard' onClick={getAPI}>New Color Scheme</button>
     </div>
     
     <div className='component-display'>
       <div className='colors-container'>
-          {schemeArr === '' ? <p className='no-colors'>Click "New Color Scheme" to view colors.</p> : colorSchemeLoop()}
-          {schemeArr.split(',').length%3 === 1 || schemeArr.split(',').length%3 === 2 ? <div aria-hidden='true' className='container-filler'></div> : null}
-          {schemeArr.split(',').length%3 === 1 ? <div aria-hidden='true' className='container-filler'></div> : null}
+          {schemeArr.length === 0 ? <p className='no-colors'>Click "New Color Scheme" to view colors.</p> : colorSchemeLoop()}
+          {schemeArr.length%3 === 1 || schemeArr.length%3 === 2 ? <div aria-hidden='true' className='container-filler'></div> : null}
+          {schemeArr.length%3 === 1 ? <div aria-hidden='true' className='container-filler'></div> : null}
       </div>      
     </div>
   </div>

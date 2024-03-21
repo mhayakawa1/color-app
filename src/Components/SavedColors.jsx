a836ec8f0086f1471888734abe1570fea1df8b3b
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2024-03-20 21:44:19 -0700

    Update SavedColors.jsx

--- src/Components/SavedColors.jsx
+++ src/Components/SavedColors.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function SavedColors(props){
   const [savedColors, setSavedColors] = useState('');
   const [showConfirm, setShowConfirm] = useState(false);
-  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+  const hexVals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
   const [lastDeleted, setLastDeleted] = useState('');//create undo delete color feature?
 
   const clearAll = (str) => {
@@ -21,73 +21,39 @@ export default function SavedColors(props){
 
   const savedColorsLoop = () => {
     let itemsArr = []
-  {/*  //convert rgb to hex
-    let rgbInput = savedColors.split('*').splice(1)
-    let hexOutput = []
-    for(let i = 0; i < rgbInput.length; i++){
-      let itemSplit = rgbInput[i].split(',');
-      let itemStr = ''
-      for(let j = 0; j < itemSplit.length; j++){
-        itemSplit[j] = itemSplit[j]/16
-        itemSplit[j] = itemSplit[j].toString().split('.')
-        itemSplit[j][1] = '.' + itemSplit[j][1]
-        if(itemSplit[j][1] === '.undefined'){
-          itemStr = itemStr + hex[itemSplit[j][0]] + '0'
+    for(let i = 0; i < props.data.length; i++){
+      let rgbInput = [...props.data[i]]
+      let hexOutput = []
+      for(let j = 0; j < 3; j++){
+        rgbInput[j] = rgbInput[j]/16
+        rgbInput[j] = rgbInput[j].toString().split('.')
+        rgbInput[j][1] = '.' + rgbInput[j][1]
+        if(rgbInput[j][1] === '.undefined'){
+          hexOutput.push(hexVals[rgbInput[j][0]], '0')
         }else{
-          itemStr = itemStr + hex[itemSplit[j][0]] + hex[itemSplit[j][1]*16]
+          hexOutput.push(hexVals[rgbInput[j][0]], hexVals[rgbInput[j][1]*16])
         }
       }
-      hexOutput.push(itemStr.toUpperCase())
-    }
 
-    let splitArr = savedColors.split('*');//array from savedColors, split into multiple arrays
-
-    const deleteColor = (rgb) => {
-      splitArr = splitArr.filter(elem => elem !== rgb)
-      splitArr = splitArr.join('*')
-      setSavedColors(splitArr)
-      localStorage.setItem('savedColors', splitArr);
-    }
-    for(let i = 0; i < splitArr.length; i++){
-      //replace all elements in splitArr with array made of each element
-      splitArr.splice(i, 1, splitArr[i].split(','))
-    }
-    for(let i = 0; i < splitArr.length; i++){
-      const item = <div key={`(${splitArr[i][0]}, ${splitArr[i][1]}, ${splitArr[i][2]})`} className='color-item'>
-        <div className='color-block' style={{background: `rgb(${splitArr[i][0]}, ${splitArr[i][1]}, ${splitArr[i][2]})`}}>
+      const item = <div key={props.data[i]} className='color-item'>
+        <div className='color-block' style={{backgroundColor: `rgb(${props.data[i].join(',')})`}}>
         </div>
         <div className='color-info'>
-            <p className='color-value'>{`RGB: (${splitArr[i][0]},${splitArr[i][1]},${splitArr[i][2]})`}</p>
-            <p className='color-value'>HEX: #{hexOutput[i-1]}</p>
-            <button className='btn-standard btn-small' onClick={() => deleteColor(splitArr[i])}>Delete</button>
-        </div>
-      </div>   
-      if(splitArr[i][0] !== ''){
-        itemsArr.push(item)
-      }
-    }*/}
-    for(let i = 0; i < props.data.length; i++){
-      const item = <div key={props.data[i].join('')} className='color-item'>
-        <div className='color-block' style={{background: `rgb(${props.data[i].join('')}}`}}>
-        </div>
-        <div className='color-info'>
-            <p className='color-value'>{`RGB: (${props.data.join('')})`}</p>
-            <p className='color-value'>HEX: #</p>
+            <p className='color-value'>{`RGB: (${props.data[i].join(',')})`}</p>
+            <p className='color-value'>HEX: #{hexOutput.join('').toUpperCase()}</p>
             <button className='btn-standard btn-small' 
             //onClick={() => deleteColor(splitArr[i])}
             >Delete</button>
         </div>
-      </div>   
-      if(props.data.length > 0){
+      </div>
         itemsArr.push(item)
-      }{/**/}
     }
     
     return(
       itemsArr
     )
   }
-//console.log('sc: ', props.data)
+
   return(    
   <div className='component-container-2'>
     <div className='controls-container'>
@@ -106,9 +72,9 @@ export default function SavedColors(props){
 
     <div className='component-display'>
       <div className='colors-container'>
-        {savedColors.length === 0 ? <p className='no-colors'>You have no saved colors.</p> : savedColorsLoop()}
-        {savedColors.split('*').splice(1).length%3 === 1 || savedColors.split('*').splice(1).length%3 === 2 ? <div aria-hidden='true' className='container-filler'></div> : null}
-        {savedColors.split('*').splice(1).length%3 === 1 ? <div aria-hidden='true' className='container-filler'></div> : null}
+        {props.data.length === 0 ? <p className='no-colors'>You have no saved colors.</p> : savedColorsLoop()}
+        {props.data.length%3 === 1 || props.data.length%3 === 2 ? <div aria-hidden='true' className='container-filler'></div> : null}
+        {props.data.length%3 === 1 ? <div aria-hidden='true' className='container-filler'></div> : null}
       </div>
     </div>
   </div>

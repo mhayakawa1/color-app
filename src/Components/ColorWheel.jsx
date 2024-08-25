7f7538d6784e1bc62d328d9b938a50edafdd04ad
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2024-08-25 15:32:58 -0700

    Update ColorWheel.jsx

--- src/Components/ColorWheel.jsx
+++ src/Components/ColorWheel.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export default function ColorWheel(){
-  const [schemeType, setSchemeType] = useState('monochromatic')
+  const [schemeType, setSchemeType] = useState('monochromatic');
   const [one, setOne] = useState(false);
   const [classesArr2, setClassesArr2] = useState({
     0: false,
@@ -17,15 +17,29 @@ export default function ColorWheel(){
     10: false,
     11: false
   })
-  let classesArr = ['red', 'redOrange', 'orange', 'orangeYellow', 
-  'yellow', 'yellowGreen', 'green', 'greenBlue',
-  'blue', 'bluePurple', 'purple', 'purpleRed']
-  let colorsArr = ['red', 'redOrange', 'orange', 'orangeYellow', 
-  'yellow', 'yellowGreen', 'green', 'greenBlue',
-  'blue', 'bluePurple', 'purple', 'purpleRed']
+  let classesArr = ['red', 'red-orange', 'orange', 'orange-yellow', 
+  'yellow', 'yellow-green', 'green', 'green-glue',
+  'blue', 'blue-purple', 'purple', 'purple-red'];
+  let colorsArr = ['red', 'red-orange', 'orange', 'orange-yellow', 
+  'yellow', 'yellow-green', 'green', 'green-blue',
+  'blue', 'blue-purple', 'purple', 'purple-red'];
   let colorNamesArr = ['Red', 'Red Orange', 'Orange', 'Orange Yellow', 
   'Yellow', 'Yellow Green', 'Green', 'Green Blue',
-  'Blue', 'Blue Purple', 'Purple', 'Purple Red']
+  'Blue', 'Blue Purple', 'Purple', 'Purple Red'];
+  const colorsInfo = {
+    'red': false,
+    'red-orange': false,
+    'orange': false,
+    'orange-yellow': false,
+    'yellow': false,
+    'yellow-green': false,
+    'green': false,
+    'green-blue': false,
+    'blue': false,
+    'blue-purple': false,
+    'purple': false,
+    'purple-red': false
+  }
   let selectedColors = [];
 
   const handleDropdown = (event) => {
@@ -33,7 +47,9 @@ export default function ColorWheel(){
   }
 
   const findColorScheme = (colorStr) => {
+    console.log(colorsArr)
     colorsArr = [...colorsArr.splice(colorsArr.indexOf(colorStr)), ...colorsArr.splice(0)];
+    console.log(colorsArr)
     switch(schemeType){
       case 'monochromatic':
         selectedColors = [colorsArr[0]];
@@ -56,8 +72,8 @@ export default function ColorWheel(){
 
     setOne(i => !i)
 
-    for(let i = 0; i < classesArr.length; i++){
-      if(selectedColors.includes(classesArr[i]) === true){        
+    for(let i = 0; i < colorsArr.length; i++){
+      if(selectedColors.includes(colorsArr[i]) === true){        
         classesArr2[i] = true
       }else{
         classesArr2[i] = false
@@ -68,10 +84,10 @@ export default function ColorWheel(){
   const colorNamesLoop = () => {
     let renderColorNames = []
 
-    for(let i = 0; i < colorNamesArr.length; i++){
+    for(let i = 0; i < colorsArr.length; i++){
       if(classesArr2[i] === true){
         renderColorNames.push(
-          <li key={i}>{colorNamesArr[i]}</li>
+          <li key={i}>{colorsArr[i].split('-').map((i) => i[0].toUpperCase() + i.substring(1, i.length)).join(' ')}</li>
         )
       }
     }    
@@ -125,27 +141,27 @@ export default function ColorWheel(){
       <div className='wheel-container'>
         <button onClick={() => findColorScheme('red')} 
           className={`color-wheel-circle red ${classesArr2[0] ? 'dropshadow' : ''}`}></button>
-        <button onClick={() => findColorScheme('redOrange')}
+        <button onClick={() => findColorScheme('red-orange')}
           className={`color-wheel-circle red-orange ${classesArr2[1] ? 'dropshadow' : ''}`}></button>
         <button onClick={() => findColorScheme('orange')} 
           className={`color-wheel-circle orange ${classesArr2[2] ? 'dropshadow' : ''}`}></button>
-        <button onClick={() => findColorScheme('orangeYellow')} 
+        <button onClick={() => findColorScheme('orange-yellow')} 
           className={`color-wheel-circle orange-yellow ${classesArr2[3] ? 'dropshadow' : ''}`}></button>
         <button onClick={() => findColorScheme('yellow')} 
           className={`color-wheel-circle yellow ${classesArr2[4] ? 'dropshadow' : ''}`}></button>
-          <button onClick={() => findColorScheme('yellowGreen')} 
+          <button onClick={() => findColorScheme('yellow-green')} 
           className={`color-wheel-circle yellow-green ${classesArr2[5] ? 'dropshadow' : ''}`}></button>
         <button onClick={() => findColorScheme('green')} 
           className={`color-wheel-circle green ${classesArr2[6] ? 'dropshadow' : ''}`}></button>
-        <button onClick={() => findColorScheme('greenBlue')} 
+        <button onClick={() => findColorScheme('green-blue')} 
           className={`color-wheel-circle green-blue ${classesArr2[7] ? 'dropshadow' : ''}`}></button>
         <button onClick={() => findColorScheme('blue')} 
           className={`color-wheel-circle blue ${classesArr2[8] ? 'dropshadow' : ''}`}></button>
-        <button onClick={() => findColorScheme('bluePurple')} 
+        <button onClick={() => findColorScheme('blue-purple')} 
           className={`color-wheel-circle blue-purple ${classesArr2[9] ? 'dropshadow' : ''}`}></button>
         <button onClick={() => findColorScheme('purple')} 
           className={`color-wheel-circle purple ${classesArr2[10] ? 'dropshadow' : ''}`}></button>
-        <button onClick={() => findColorScheme('purpleRed')} 
+        <button onClick={() => findColorScheme('purple-red')} 
           className={`color-wheel-circle purple-red ${classesArr2[11] ? 'dropshadow' : ''}`}></button>
       </div>
       <ul className='color-names-2'>

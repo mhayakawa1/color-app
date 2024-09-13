a6146cdbb9cfe46a2eef57b717b63cada85d02a9
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2024-09-12 21:40:07 -0700

    Update ColorPicker.jsx

--- src/Components/ColorPicker.jsx
+++ src/Components/ColorPicker.jsx
@@ -1,52 +1,54 @@
 import React, { useState } from 'react';
 
 export default function ColorPicker(props){
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
   const [hexCode, setHexCode] = useState('');
   const [displayHexCode, setDisplayHexCode] = useState('');
   const hexVals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
-  const rgbValues = [red, green, blue];
+  const [rgbArray, setRGBArray ] = useState([0,0,0]);
+  const [rgb, setRGB] = useState([
+    {
+      color: 'red',
+      value: 0
+    },
+    {
+      color: 'green',
+      value: 0
+    },
+    {
+      color: 'blue',
+      value: 0
+    }
+  ])
+  const [rgbCode, setRGBCode] = useState(`0,0,0`);
 
   const changeColor = (array) => {
     setHexCode('');
     setDisplayHexCode('');
-    if(array[0] !== undefined){
-      setRed(array[0]);
+
+    let newRGB = [...rgb];
+    for(let i = 0; i < newRGB.length; i++){
+      if(array[i] !== undefined){
+        newRGB[i].value = array[i]
+      }
     }
-    if(array[1] !== undefined){
-      setGreen(array[1]);
-    }
-    if(array[2] !== undefined){
-      setBlue(array[2]);
-    }    
+    setRGB(newRGB);
+
+    //const rgbValues = 
+    setRGBArray([rgb[0].value, rgb[1].value, rgb[2].value])
+    setRGBCode(`${rgb[0].value},${rgb[1].value},${rgb[2].value}`);
   }
 
-  const handleChangeRGB = (event, input, index) =>{
+  const handleChangeRGB = (event, index) =>{
     const value = event.target.value;
-    setHexCode('');
-    setDisplayHexCode('');
     let numValue;
-    let redValue;
-    let greenValue;
-    let blueValue;
+    let values = [undefined, undefined, undefined];
+    values[index] = value;
+    changeColor(values);
 
     if(value[0] === 0 && value.length > 1){
       numValue = 0;
     }else if(value >= 0 && value <= 255){
-      numValue = value;
-    }
-    console.log(input)
-    switch(input){
-      case 'red':
-        setRed(value);
-        break;
-      case 'green':
-        setGreen(value);
-        break;
-      case 'blue':
-        setBlue(value);
+      numValue = value
     }
   }
 
@@ -72,24 +74,23 @@ export default function ColorPicker(props){
       for(let i = 0; i < hexInput.length; i = i + 2){
         rgbOutput.push(hexInput[i]*16 + hexInput[i+1])
       }
-      
-      setRed(rgbOutput[0]);
-      setGreen(rgbOutput[1]);
-      setBlue(rgbOutput[2]);
+
+      for(let i = 0; i < rgb.length; i++){
+        rgb[i].value = rgbOutput[i]
+      }
     }
   }
 
   const rgbToHex = () =>{
-    let rgbInput = [...rgbValues];
     let hexOutput = [];
-    for(let i = 0; i < rgbInput.length; i++){
-      rgbInput[i] = rgbInput[i]/16;
-      rgbInput[i] = rgbInput[i].toString().split('.');
-      rgbInput[i][1] = '.' + rgbInput[i][1];
-      if(rgbInput[i][1] === '.undefined'){
-        hexOutput.push(hexVals[rgbInput[i][0]], '0');
+    for(let i = 0; i < rgbArray.length; i++){
+      rgbArray[i] = rgbArray[i]/16;
+      rgbArray[i] = rgbArray[i].toString().split('.');
+      rgbArray[i][1] = '.' + rgbArray[i][1];
+      if(rgbArray[i][1] === '.undefined'){
+        hexOutput.push(hexVals[rgbArray[i][0]], '0');
       }else{
-        hexOutput.push(hexVals[rgbInput[i][0]], hexVals[rgbInput[i][1]*16]);
+        hexOutput.push(hexVals[rgbArray[i][0]], hexVals[rgbArray[i][1]*16]);
       }
     }
     setHexCode(hexOutput.join(''));
@@ -97,9 +98,8 @@ export default function ColorPicker(props){
   }
   
   const saveColor = () => {
-    let color = [...rgbValues];
-    if(props.data.includes(color) === false){
-      props.clickHandler(color, false, false);
+    if(props.data.includes(rgbArray) === false){
+      props.clickHandler(rgbArray, false, false);
     }
   }
 
@@ -108,24 +108,24 @@ export default function ColorPicker(props){
     <div className='controls-container color-picker-controls'>
       <div className='input-container'>
         <div className='input-item'>
-          <input type='range' min='0' max='255' value={red === '' ? 0 : red} className='slider'
-            onChange={event => handleChangeRGB(event, 'red', 0)}/>
-          <input className='input-number' type='number' value={red === '' ? 0 : red}
-            onChange={event => handleChangeRGB(event, 'red', 0)}/>
+          <input type='range' min='0' max='255' value={rgb[0].value} className='slider'
+            onChange={event => handleChangeRGB(event, 0)}/>
+          <input className='input-number' type='number' value={rgb[0].value}
+            onChange={event => handleChangeRGB(event, 0)}/>
           <label>Red</label>
         </div>
         <div className='input-item'>
-          <input type='range' min='0' max='255' value={green === '' ? 0 : green} className='slider'
-            onChange={event => handleChangeRGB(event, 'green', 1)}/>
-          <input className='input-number' type='number' value={green === '' ? 0 : green}
-            onChange={event => handleChangeRGB(event, 'green', 1)}/>
+          <input type='range' min='0' max='255' value={rgb[1].value} className='slider'
+            onChange={event => handleChangeRGB(event, 1)}/>
+          <input className='input-number' type='number' value={rgb[1].value}
+            onChange={event => handleChangeRGB(event, 1)}/>
           <label>Green</label>
         </div>
         <div className='input-item'>
-          <input type='range' min='0' max='255' value={blue === '' ? 0 : blue} className='slider'
-            onChange={event => handleChangeRGB(event, 'blue', 2)}/>
-          <input className='input-number' type='number' value={blue === '' ? 0 : blue}
-            onChange={event => handleChangeRGB(event, 'blue', 2)}/>
+          <input type='range' min='0' max='255' value={rgb[2].value} className='slider'
+            onChange={event => handleChangeRGB(event, 2)}/>
+          <input className='input-number' type='number' value={rgb[2].value}
+            onChange={event => handleChangeRGB(event, 2)}/>
           <label>Blue</label>
         </div>
       </div>
@@ -137,19 +137,19 @@ export default function ColorPicker(props){
       </div>
       
       <div className='reset-save'>
-        <button className='btn-standard reset' onClick={() => changeColor(0,0,0)}>Reset</button>
+        <button className='btn-standard reset' onClick={() => changeColor([0,0,0])}>Reset</button>
         <button className='btn-standard save'onClick={saveColor}>Save Color</button>
       </div>
       <button className='random-color' onClick={() => changeColor([Math.round(Math.random() * (255)),Math.round(Math.random() * (255)),Math.round(Math.random() * (255))])}
-        style={{filter: `drop-shadow(0px 0px 4px rgb(${red === '' ? 0 : red},${green === '' ? 0 : green},${blue === '' ? 0 : blue}))`,
-          borderColor: `rgb(${red === '' ? 0 : red},${green === '' ? 0 : green},${blue === '' ? 0 : blue})`}}>Random<br/>Color</button>
+        style={{filter: `drop-shadow(0px 0px 4px rgb(${rgbCode}))`,
+          borderColor: `rgb(${rgbCode})`}}>Random<br/>Color</button>
     </div>
     
     <div className='component-display custom-color-display'>
         <div className='custom-color-block'
-          style={{backgroundColor: `rgb(${red === '' ? 0 : red},${green === '' ? 0 : green},${blue === '' ? 0 : blue})`}}>
+          style={{backgroundColor: `rgb(${rgbCode})`}}>
           <div className='custom-color-info'>
-            <p>{`RGB: (${red === '' ? 0 : red},${green === '' ? 0 : green},${blue === '' ? 0 : blue})`}</p>
+            <p>{`RGB: (${rgbCode})`}</p>
             <p>HEX: {displayHexCode === '' ? '-' : `#${displayHexCode.toUpperCase()}`}</p>
           </div>
         </div>

a0054cd415e4d71ce74d8fcd456993fc603d4f17
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-01-28 11:27:22 -0700

    Create MenuButtons.jsx

---
+++ src/Components/MenuButtons.jsx
@@ -0,0 +1,28 @@
+import ColorPicker from "./ColorPicker";
+import ColorWheel from "./ColorWheel";
+import SavedColors from "./SavedColors";
+
+const MenuButtons = ({ switchComponent }) => {
+  let buttons = [];
+  
+  const components = [
+    { component: <SavedColors />, name: "Saved Colors", margin: "0" },
+    { component: <ColorPicker />, name: "ColorPicker", margin: "0 0 0 25%" },
+    { component: <ColorWheel />, name: "Color Wheel", margin: "0 0 0 50%" },
+  ];
+  
+  for (let i = 0; i < components.length; i++) {
+    buttons.push(
+      <button
+        key={i}
+        className="menu-button"
+        onClick={() => switchComponent(components[i])}
+      >
+        {components[i].name}
+      </button>
+    );
+  }
+  return buttons;
+};
+
+export default MenuButtons;

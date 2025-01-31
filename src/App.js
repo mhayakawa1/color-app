e17533cc85159bfa66278085d7b22d84c7dc8e0f
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-01-30 21:58:41 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,116 +1,74 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import ColorPicker from './Components/ColorPicker';
-import ColorWheel from './Components/ColorWheel';
-import SavedColors from './Components/SavedColors';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-
-const MenuButton = ({switchComponent, component}) => {
-  return (
-    <button className='menu-button' onClick={() => switchComponent(component)}>{component}</button>
-  )
-}
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { ColorsProvider} from "./Contexts/ColorsContext";
+import ColorPicker from "./Components/ColorPicker";
+import ColorWheel from "./Components/ColorWheel";
+import SavedColors from "./Components/SavedColors";
+import Menu from "./Components/Menu";
+import MenuButtons from "./Components/MenuButtons";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function App() {
-  const [savedColors, setSavedColors] = useState([]);
   const [display, setDisplay] = useState('Saved Colors');
   const [isMenuVisible, setIsMenuVisible] = useState(false);
-  const [saveColorData, setSaveColorData] = useState('');
-
-  const onStorageUpdate = (e) => {
-    const { key, newValue } = e;
-    if (key === 'saveColorData') {
-      setSaveColorData(newValue);
-    }
-  };
-
-  useEffect(() => {
-    setSaveColorData(localStorage.getItem('saveColorData') || '');
-
-    if (localStorage.getItem('saveColorData') !== null) {
-      if (localStorage.getItem('saveColorData').length > 0) {
-        setSavedColors(JSON.parse(localStorage.getItem('saveColorData')));
-      }
-    }
-    window.addEventListener('storage', onStorageUpdate);
-    return () => {
-      window.removeEventListener('storage', onStorageUpdate);
-    };
-  }, []);
-
-  function switchComponent(comp) {
-    setDisplay(comp)
+  
+  function switchComponent(component) {
+    setDisplay(component);
   }
 
   function toggleMenu() {
-    setIsMenuVisible(!isMenuVisible)
-  }
-
-  const save = (colors) => {
-    setSavedColors(colors);
-    setSaveColorData(colors.join(' '));
-    localStorage.setItem('saveColorData', JSON.stringify(colors));
-  }
-
-  const handleClick = (colorInput, multipleColors, deleteColor) => {
-    let add = [];
-    const includesNewColor = savedColors.filter((i) => i !== colorInput).length === 0;
-    if (!deleteColor && includesNewColor) {
-      if (!multipleColors) {
-        add.push(colorInput);
-      } else {
-        add = [...add, ...colorInput];
-      }
-      save(savedColors.concat(add));
-    } else if(deleteColor){
-      save([]);
-    }
-  }
-
-  const renderMenuButtons = () => {
-    let buttons = [];
-    const components = ['Saved Colors', 'Color Picker', 'Color Wheel'];
-    for (let i = 0; i < components.length; i++) {
-      buttons.push(
-        <MenuButton key={i} switchComponent={switchComponent} component={components[i]} />
-      )
-    }
-    return (
-      buttons
-    )
+    setIsMenuVisible(!isMenuVisible);
   }
 
   return (
-    <main className='app'>
-      <div className='menu-container'>
-        <div className='menu'>
-          <div className={`menu-buttons`}>
-            {renderMenuButtons()}
+    <ColorsProvider>
+      <main className="app">
+        {/* <div className="menu-container">
+          <div className="menu">
+            <div className={`menu-buttons`}>
+              <MenuButtons switchComponent={switchComponent} />
+            </div>
+            <span
+              className="active-bar"
+              style={
+                display === 'Saved Colors'
+                  ? { margin: "0" }
+                  : display === 'Color Picker'
+                  ? { margin: "0 0 0 25%" }
+                  : { margin: "0 0 0 50%" }
+              }
+            ></span>
           </div>
-          <span className='active-bar' style={
-            display === 'Saved Colors' ? { margin: '0' }
-              : display === 'Color Picker' ? { margin: '0 0 0 25%' }
-                : { margin: '0 0 0 50%' }
-          }>
-          </span>
+        </div> */}
+        <Menu />
+        <div className="mobile-menu">
+          <button className="dropdown-button" onClickCapture={toggleMenu}>
+            {!isMenuVisible ? (
+              <AiOutlineMenu className="icon"></AiOutlineMenu>
+            ) : (
+              <AiOutlineClose className="icon"></AiOutlineClose>
+            )}
+          </button>
+          <div
+            className={`menu-buttons ${
+              isMenuVisible ? "menu-buttons-height fade-in" : "fade-out"
+            }`}
+          >
+            <MenuButtons switchComponent={switchComponent} />
+          </div>
         </div>
-      </div>
-      <div className='mobile-menu'>
-        <button className='dropdown-button' onClickCapture={toggleMenu}>
-          {!isMenuVisible ? <AiOutlineMenu className='icon'></AiOutlineMenu> :
-            <AiOutlineClose className='icon'></AiOutlineClose>}
-        </button>
-        <div className={`menu-buttons ${isMenuVisible ? 'menu-buttons-height fade-in' : 'fade-out'}`}>
-          {renderMenuButtons()}
-        </div>
-      </div>
 
-      <div className='component-container'>
-        {display === 'Saved Colors' ? <SavedColors clickHandler={handleClick} data={savedColors} />
-          : display === 'Color Picker' ? <ColorPicker clickHandler={handleClick} data={savedColors} />
-            : <ColorWheel />}
-      </div>
-    </main>
+        <div className="component-container">
+          {display === "Saved Colors" ? (
+            <SavedColors />
+          ) : display === "Color Picker" ? (
+            <ColorPicker />
+          ) : (
+            <ColorWheel />
+          )}
+        </div>
+      </main>
+    </ColorsProvider>
   );
 }
 

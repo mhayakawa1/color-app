fcf4c932943d146c93d254444cf13aa1e4cfd813
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-02-05 08:34:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,17 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import { ColorsProvider} from "./Contexts/ColorsContext";
-import ColorPicker from "./Components/ColorPicker";
-import ColorWheel from "./Components/ColorWheel";
-import SavedColors from "./Components/SavedColors";
+import React, { useState } from "react";
+import { ColorsProvider } from "./Contexts/ColorsContext";
+import ComponentContainer from "./Components/ComponentContainer";
 import Menu from "./Components/Menu";
-import MenuButtons from "./Components/MenuButtons";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import MobileMenu from "./Components/MobileMenu";
 
 function App() {
-  const [display, setDisplay] = useState('Saved Colors');
-  const [isMenuVisible, setIsMenuVisible] = useState(false);
-  
-  function switchComponent(component) {
-    setDisplay(component);
-  }
-
-  function toggleMenu() {
-    setIsMenuVisible(!isMenuVisible);
-  }
-
   return (
     <ColorsProvider>
       <main className="app">
-        {/* <div className="menu-container">
-          <div className="menu">
-            <div className={`menu-buttons`}>
-              <MenuButtons switchComponent={switchComponent} />
-            </div>
-            <span
-              className="active-bar"
-              style={
-                display === 'Saved Colors'
-                  ? { margin: "0" }
-                  : display === 'Color Picker'
-                  ? { margin: "0 0 0 25%" }
-                  : { margin: "0 0 0 50%" }
-              }
-            ></span>
-          </div>
-        </div> */}
         <Menu />
-        <div className="mobile-menu">
-          <button className="dropdown-button" onClickCapture={toggleMenu}>
-            {!isMenuVisible ? (
-              <AiOutlineMenu className="icon"></AiOutlineMenu>
-            ) : (
-              <AiOutlineClose className="icon"></AiOutlineClose>
-            )}
-          </button>
-          <div
-            className={`menu-buttons ${
-              isMenuVisible ? "menu-buttons-height fade-in" : "fade-out"
-            }`}
-          >
-            <MenuButtons switchComponent={switchComponent} />
-          </div>
-        </div>
-
-        <div className="component-container">
-          {display === "Saved Colors" ? (
-            <SavedColors />
-          ) : display === "Color Picker" ? (
-            <ColorPicker />
-          ) : (
-            <ColorWheel />
-          )}
-        </div>
+        <MobileMenu />
+        <ComponentContainer />
       </main>
     </ColorsProvider>
   );

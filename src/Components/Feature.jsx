9805a1ba118212469c53b70f367123d5ea59e0f0
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-07-23 20:21:21 -0700

    Update Feature.jsx

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,8 +1,3 @@
-import React from 'react';
-
 export default function Feature(props) {
-
-    return (
-        <div className={`feature ${props.className}`}>{props.children}</div>
-    )
+  return <div className={`feature ${props.className}`}>{props.children}</div>;
 }

7cb311a7af6fc8f9802f6cb48e126fbd0d512437
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-07-28 12:05:19 -0700

    Update ColorsContext.jsx

--- src/Contexts/ColorsContext.jsx
+++ src/Contexts/ColorsContext.jsx
@@ -10,11 +10,31 @@ export function useColors() {
 
 export const ColorsProvider = ({ children }) => {
   const [savedColors, setSavedColors] = useState([]);
+  const [isCopiedVisible, setIsCopiedVisible] = useState(false);
+  const [copiedFromSaved, setCopiedFromSaved] = useState(true);
   const [displayData, setDisplay] = useState({
     component: <SavedColors />,
     name: "Saved Colors",
     margin: "0",
   });
+  const hexCharacters = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
 
   const onStorageUpdate = (e) => {
     const { key, newValue } = e;
@@ -56,9 +76,44 @@ export const ColorsProvider = ({ children }) => {
     }
   };
 
+  const convertHexToRGB = (inputValue) => {
+    let hexInput = inputValue.split("");
+    for (let i = 0; i < hexInput.length; i++) {
+      if (/[0-9]/.test(hexInput[i]) === false) {
+        hexInput.splice(i, 1, hexCharacters.indexOf(hexInput[i]));
+      } else {
+        hexInput.splice(i, 1, Number(hexInput[i]));
+      }
+    }
+    let rgbOutput = [];
+    for (let i = 0; i < hexInput.length; i = i + 2) {
+      rgbOutput.push(hexInput[i] * 16 + hexInput[i + 1]);
+    }
+    return rgbOutput;
+  };
+
+  const copyText = (colorCode, copiedFromSaved) => {
+    navigator.clipboard.writeText(colorCode);
+    setCopiedFromSaved(copiedFromSaved);
+    setIsCopiedVisible(true);
+    setTimeout(() => {
+      setIsCopiedVisible(false);
+    }, 3000);
+  };
+
   return (
     <ColorsContext.Provider
-      value={{ savedColors, updateColors, switchComponent, displayData }}
+      value={{
+        savedColors,
+        isCopiedVisible,
+        updateColors,
+        switchComponent,
+        displayData,
+        copyText,
+        copiedFromSaved,
+        hexCharacters,
+        convertHexToRGB,
+      }}
     >
       {children}
     </ColorsContext.Provider>

d7bd38f83cd5d06b2a7dffc47f02c22ef94e87df
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-09-25 06:50:10 -0700

    Update Menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,19 +1,24 @@
 import { useColors } from "../Contexts/ColorsContext";
 import MenuButtons from "./MenuButtons";
+import Logo from "../MyColorsLogo.png";
 
 const Menu = () => {
   const { switchComponent, displayData } = useColors();
 
   return (
     <div className="menu-container">
+      <div className="logo-container">
+        <img src={Logo} alt="" />
+        <span>MyColors</span>
+      </div>
       <div className="menu">
+        <span
+          className="active-bar"
+          style={{ margin: displayData.margin }}
+        ></span>
         <div className={`menu-buttons`}>
           <MenuButtons switchComponent={switchComponent} />
         </div>
-        <span
-          className="active-bar"
-          style={{margin: displayData.margin}}
-        ></span>
       </div>
     </div>
   );

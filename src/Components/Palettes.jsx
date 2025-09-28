9e5adf8adefd6b058f1ed7f2c233c405e84b51ae
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-09-28 08:56:44 -0700

    Create Palettes.jsx

---
+++ src/Components/Palettes.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import NoData from "./NoData";
+
+export default function Palettes() {
+  return (
+    <div className="palettes">
+      <NoData string="palettes" />
+    </div>
+  );
+}

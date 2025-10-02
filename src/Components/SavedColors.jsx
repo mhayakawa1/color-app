0c2f87c33eb3632f743c8e9bebd25267f50d798c
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-10-02 07:36:48 -0700

    Update SavedColors.jsx

--- src/Components/SavedColors.jsx
+++ src/Components/SavedColors.jsx
@@ -5,18 +5,24 @@ import Feature from "./Feature";
 import Controls from "./Controls";
 import Display from "./Display";
 import ColorItem from "./ColorItem";
-import Filler from "./Filler";
+import SingleColors from "./SingleColors";
+import Palettes from "./Palettes";
 
 export default function SavedColors() {
   const {
-    savedColors,
+    singleColors,
     updateColors,
     isCopiedVisible,
     copiedFromSaved,
     hexCharacters,
   } = useColors();
+  const [singleColorsActive, setSingleColorsActive] = useState(true);
   const [isConfirmVisible, setIsConfirmVisible] = useState(false);
-  const savedColorsLength = savedColors.length;
+  const savedColorsLength = singleColors.length;
+
+  const toggleDisplay = (boolean) => {
+    setSingleColorsActive(boolean);
+  };
 
   const clearAll = (input) => {
     switch (input) {
@@ -34,81 +40,56 @@ export default function SavedColors() {
         setIsConfirmVisible(false);
     }
   };
-
-  const colorBoxes = () => {
-    let boxes = [];
-    for (let i = 0; i < savedColorsLength; i++) {
-      let rgbValues = [...savedColors[i]];
-      let hexValues = [];
-      for (let j = 0; j < 3; j++) {
-        rgbValues[j] = rgbValues[j] / 16;
-        rgbValues[j] = rgbValues[j].toString().split(".");
-        rgbValues[j][1] = "." + rgbValues[j][1];
-        if (rgbValues[j][1] === ".undefined") {
-          hexValues.push(hexCharacters[rgbValues[j][0]], "0");
-        } else {
-          hexValues.push(
-            hexCharacters[rgbValues[j][0]],
-            hexCharacters[rgbValues[j][1] * 16]
-          );
-        }
-      }
-      boxes.push(
-        <ColorItem
-          key={i}
-          color={savedColors[i]}
-          hex={hexValues.join("").toUpperCase()}
-          clickHandler={updateColors}
-          argumentList={[savedColors[i], true]}
-          buttonText="Delete"
-        />
-      );
-    }
-    return boxes;
-  };
+  
   return (
     <Feature className="saved-colors">
       <Controls>
-        <Button
-          text="Clear All"
-          handleClick={
-            savedColorsLength !== 0 ? () => clearAll("clear all") : undefined
-          }
-          className={`standard ${savedColorsLength === 0 && "disabled"}`}
-        />
-        <div
-          className={isConfirmVisible ? "confirm-clear-all" : "hide-confirm"}
-        >
-          <p>Are you sure you want to clear all colors?</p>
-          <div className="confirm-btns">
-            <Button
-              text="Yes"
-              handleClick={() => clearAll(true)}
-              className="standard secondary button-small"
-            />
-            <Button
-              text="No"
-              handleClick={() => clearAll(false)}
-              className="standard secondary button-small"
-            />
+        <div>
+          <Button
+            text="Clear All"
+            handleClick={
+              savedColorsLength !== 0 ? () => clearAll("clear all") : undefined
+            }
+            className={`standard ${savedColorsLength === 0 && "disabled"}`}
+          />
+          <div
+            className={isConfirmVisible ? "confirm-clear-all" : "hide-confirm"}
+          >
+            <p>Are you sure you want to clear all colors?</p>
+            <div className="confirm-buttons">
+              <Button
+                text="Yes"
+                handleClick={() => clearAll(true)}
+                className="standard secondary button-small"
+              />
+              <Button
+                text="No"
+                handleClick={() => clearAll(false)}
+                className="standard secondary button-small"
+              />
+            </div>
           </div>
+          {isCopiedVisible && copiedFromSaved && (
+            <p className={`copied fade-copied`}>Copied to clickboard!</p>
+          )}
         </div>
-        {isCopiedVisible && copiedFromSaved && (
-          <p className={`copied fade-copied`}>Copied to clickboard!</p>
-        )}
+        <div className="toggle-buttons">
+          <button
+            className={`standard button-small ${singleColorsActive ? "active" : 'inactive'}`}
+            onClick={() => toggleDisplay(true)}
+          >
+            Single Colors
+          </button>
+          <button
+            className={`standard button-small ${!singleColorsActive ? "active" : 'inactive'}`}
+            onClick={() => toggleDisplay(false)}
+          >
+            Palettes
+          </button>
+        </div>
       </Controls>
       <Display>
-        <div className="colors-container">
-          {savedColorsLength === 0 ? (
-            <p className="no-colors">You have no saved colors.</p>
-          ) : (
-            colorBoxes()
-          )}
-          {(savedColorsLength % 3 === 1 || savedColorsLength % 3 === 2) && (
-            <Filler />
-          )}
-          {savedColorsLength % 3 === 1 && <Filler />}
-        </div>
+        {singleColorsActive ? <SingleColors /> : <Palettes />}
       </Display>
     </Feature>
   );

6da69490c24f2a3a5723d2ab31f9e9498b135093
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2026-05-20 06:14:11 -0700

    Update DeleteModal.jsx

--- src/Components/DeleteModal.jsx
+++ src/Components/DeleteModal.jsx
@@ -1,13 +1,24 @@
-export default function DeleteModal({ toggleModal }) {
+import { useEffect } from "react";
+
+export default function DeleteModal({ singleItem, toggleModal }) {
   return (
     <div className="delete-modal">
       <div className="confirm-container">
-        <p>Are you sure you want to delete this item?</p>
+        <p>
+          Are you sure you want to delete 
+          {singleItem ? " this item?" : " all items?"}
+        </p>
         <div className="confirm-buttons">
-          <button onClick={()=>toggleModal(null, true)} className="standard button-small">
+          <button
+            onClick={() => toggleModal(null, true, true)}
+            className="standard button-small"
+          >
             Yes
           </button>
-          <button onClick={()=>toggleModal(null, false)} className="standard button-small">
+          <button
+            onClick={() => toggleModal(null, false, false)}
+            className="standard button-small"
+          >
             No
           </button>
         </div>
